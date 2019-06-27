aa3f5dc9588784f363e87b396a26b7d7e1080776
wzamites <bj_zamites@comcast.net>
2019-06-27 15:43:03 -0400

    delete the unncessary array

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-  const diefaces = [];
-  for (var i = 1; i <= 6; i++) {
-    diefaces.push('images/dice' + i + '.png');
-  }
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+var player1roll = getRandomInt(1, 6);
+var player2roll = getRandomInt(1, 6);
 
-  var player1roll = getRandomInt(1, 6);
-  var player2roll = getRandomInt(1, 6);
-
-document.querySelector(".player1").src = diefaces[player1roll - 1];
-document.querySelector(".player2").src = diefaces[player2roll - 1];
+document.querySelector(".player1").src = 'images/dice' + player1roll + '.png';
+document.querySelector(".player2").src = 'images/dice' + player2roll + '.png';
 
 if (player1roll == player2roll) {
   document.querySelector(".result").innerText = "It's a tie";
-}
-else if (player1roll > player2roll) {
+} else if (player1roll > player2roll) {
   document.querySelector(".result").innerText = "Player 1 wins";
-}
-else if (player1roll < player2roll) {
+} else if (player1roll < player2roll) {
   document.querySelector(".result").innerText = "Player 2 Wins";
 }
